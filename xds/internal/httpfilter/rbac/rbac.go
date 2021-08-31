0e248e1be1a468ed477f466ed134bcd080d0a534
Zach Reyes <zasweq@google.com>
2021-08-31 18:17:50 -0400

    Save progress:

--- xds/internal/httpfilter/rbac/rbac.go
+++ xds/internal/httpfilter/rbac/rbac.go
@@ -23,10 +23,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	v3rbacpb "github.com/envoyproxy/go-control-plane/envoy/config/rbac/v3"
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/grpc/internal/xds/rbac"
+	"google.golang.org/grpc/metadata"
 	"strings"
 
 	//envoy_config_rbac_v3 "github.com/envoyproxy/go-control-plane/envoy/config/rbac/v3"
@@ -36,6 +36,7 @@ import (
 	"google.golang.org/grpc/xds/internal/httpfilter"
 	"google.golang.org/protobuf/types/known/anypb"
 
+	v3rbacpb "github.com/envoyproxy/go-control-plane/envoy/config/rbac/v3"
 	rpb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/rbac/v3"
 )
 
@@ -49,7 +50,7 @@ type builder struct {
 type config struct {
 	httpfilter.FilterConfig
 	config *rpb.RBAC
-	// rpb.RBACPerRoute // Contains a pointer to RBAC only
+	// rpb.RBACPerRoute // Contains a pointer to RBAC only, this needs to be looked into
 }
 
 func (builder) TypeURLs() []string {
@@ -60,6 +61,7 @@ func (builder) TypeURLs() []string {
 func parseConfig(cfg proto.Message) (httpfilter.FilterConfig, error) { // what's flow of how these are called again - look at xds.go
 	if cfg == nil {
 		// Are we sure we need this?
+		return nil, fmt.Errorf("rbax: nil configuration message provided")
 	}
 	any, ok := cfg.(*anypb.Any)
 	if !ok {
@@ -71,57 +73,28 @@ func parseConfig(cfg proto.Message) (httpfilter.FilterConfig, error) { // what's
 		return nil, fmt.Errorf("rbac: error parsing config %v: %v", cfg, err)
 	}
 
-
-	// IS THIS WHERE I PUT VALIDATION ERRORS? CHECK VALIDATION AS PER A41
-	// I'm gonna put them here
-	/*msg.Rules.Policies[0].Condition // These must cause validation failure if present, cause listener to be nacked
-	msg.Rules.Policies[0].CheckedCondition // What level is actually used to instantiate the RBAC Configuration?
-
-	// If a permission or principal has a header matcher for a grpc- prefixed
-	// header name or :scheme, it is a validation failure. - A41
-	// msg.Rules.Policies[0].Principals[0]. // Header matcherrrrrr
-	// There is many policies...how do we convert these and check them
-	msg.Rules.Policies[0].Principals[0] // Iterate through both of these for validation
-	msg.Rules.Policies[0].Permissions[0]*/
-
-	// How this gets interfaced with? Validation will get piped back to NACK Listener eventually
-
-	// Iterate through policies
 	for _, policy := range msg.Rules.Policies {
-		// check for condition and checked condition in each one
-
 		// "Policy.condition and Policy.checked_condition must cause a
 		// validation failure if present." - A41
 		if policy.Condition != nil {
-			// validation failure - return error?
 			return nil, errors.New("rbac: Policy.condition is present")
 		}
 		if policy.CheckedCondition != nil {
-			// validation failure - return error?
 			return nil, errors.New("rbac: policy.CheckedCondition is present")
 		}
 
 		// "It is also a validation failure if Permission or Principal has a
-		// header matcher for a grpc-prefixed header name or :scheme." - A41
-
-		// loop through principals
-		// check if header matcher and clause
+		// header matcher for a grpc- prefixed header name or :scheme." - A41
 		for _, principal := range policy.Principals {
-			// Look at what I wrote in RBAC Engine, get an example from that
 			if principal.GetHeader() != nil {
-				// grpc-prefixed header name or :scheme
 				name := principal.GetHeader().GetName()
 				if name == ":scheme" || strings.HasPrefix(name, "grpc-") {
 					return nil, fmt.Errorf("rbac: principal header matcher for %v is :scheme or starts with grpc", name)
 				}
 			}
 		}
-
-		// loop through permissions
-		// check if header matcher and clause
 		for _, permission := range policy.Permissions {
 			if permission.GetHeader() != nil {
-				// grpc-prefixed header name or :scheme
 				name := permission.GetHeader().GetName()
 				if name == ":scheme" || strings.HasPrefix(name, "grpc-") {
 					return nil, fmt.Errorf("rbac: permission header matcher for %v is :scheme or starts with grpc", name)
@@ -129,15 +102,6 @@ func parseConfig(cfg proto.Message) (httpfilter.FilterConfig, error) { // what's
 			}
 		}
 	}
-
-
-
-	// Look at how Ashitha calls this layer
-	// right now you take a list[] of stuff with action and long policies
-
-	// Due to the RBAC policy validation logic being specific to RBAC itself,
-	// I think this should do it's own iteration through the full proto, and then instantiate the chain engine
-	// with the single length list used to configure this
 
 	return config{config: msg}, nil
 }
@@ -148,13 +112,16 @@ func (builder) ParseFilterConfig(cfg proto.Message) (httpfilter.FilterConfig, er
 
 func (builder) ParseFilterConfigOverride(override proto.Message) (httpfilter.FilterConfig, error) {
 	// This is an RBAC Per Route configuration override, thus need to look into it for RBAC
+
+	// TODO: Look into proto.Message here, it's RBACPerRoute.RBAC
+
+	// Generic parseConfig(VVV)
 	return parseConfig(override) // Look into it somehow for RBAC?
 }
 
 var _ httpfilter.ServerInterceptorBuilder = builder{}
 
-// server side only
-// Before it even gets here, validated using ^^^?
+// Optional interface builder implements in order to signify it works server side.
 func (builder) BuildServerInterceptor(cfg httpfilter.FilterConfig, override httpfilter.FilterConfig) (resolver.ServerInterceptor, error) {
 	if cfg == nil {
 		return nil, fmt.Errorf("rbac: nil config provided")
@@ -188,15 +155,11 @@ type interceptor struct {
 }
 
 func (i *interceptor) AllowRPC(ctx context.Context) error {
-	// ":method can be hard-coded to POST if unavailable and a code audit confirms the server denies requests for all other method types"
+	// Is there any other functionality that needs to be added here? (i.e. scoped to the functionality of the xDS Configured RBAC Engine?)
 
-	// Validated and now instantiated with this config
-	// Now what behavior comes from the config into this method
-	// How does Ashitha call this?
-	// A singular action + policy list to match to.
-	// Rather than []{action + policy}{action + policy}
-	// This new chain engine thing should happen once, as this AllowRPC will be called many times in the xds interceptors
-
-	//rbac.NewChainEngine([]*v3rbacpb.RBAC{i.config.Rules}) // Do you do this once, or do it per config?
+	// ":method can be hard-coded to POST if unavailable" - A41
+	ctx = metadata.AppendToOutgoingContext(ctx, ":method", "POST")
 	return i.chainEngine.IsAuthorized(ctx)
 }
+
+// TODO ONCE GET BACK: FINISH CLEANUP PLUS THE RBAC PER ROUTE.RBAC THING
