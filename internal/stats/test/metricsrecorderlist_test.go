eb7c508dfd31d531bc96d6f4ca6e2720ffb865d0
Zach Reyes <zasweq@google.com>
2024-07-18 21:16:02 -0400

    I think finish cleanup

--- internal/stats/test/metricsrecorderlist_test.go
+++ internal/stats/test/metricsrecorderlist_test.go
@@ -20,7 +20,6 @@ package test
 
 import (
 	"context"
-	"google.golang.org/grpc/interop/grpc_testing"
 	"log"
 	"testing"
 	"time"
@@ -29,10 +28,11 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	estats "google.golang.org/grpc/experimental/stats"
 	"google.golang.org/grpc/internal"
+	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/interop/grpc_testing"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/resolver/manual"
 	"google.golang.org/grpc/serviceconfig"
-	"google.golang.org/grpc/internal/grpctest"
 )
 
 var defaultTestTimeout = 5 * time.Second
@@ -45,11 +45,6 @@ func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-// Write comment explaining what the mock stats handler actually tests...
-
-// balancer deployed as top level that registers instruments/emits telemetry on
-// operations using those package instruments...
-
 // TestMetricsRecorderList tests the metrics recorder list functionality of the
 // ClientConn. It configures a global and local stats handler Dial Option. These
 // stats handlers implement the MetricsRecorder interface. It also configures a
@@ -61,9 +56,6 @@ func (s) TestMetricsRecorderList(t *testing.T) {
 	mr := manual.NewBuilderWithScheme("test-metrics-recorder-list")
 	defer mr.Close()
 
-	// read from 5 channels of the two fake metrics recorder channels?
-
-	// Does this even work correctly? How to trigger balancer creation?
 	json := `{"loadBalancingConfig": [{"recording_load_balancer":{}}]}`
 	sc := internal.ParseServiceConfig.(func(string) *serviceconfig.ParseResult)(json)
 	mr.InitialState(resolver.State{
@@ -72,16 +64,10 @@ func (s) TestMetricsRecorderList(t *testing.T) {
 
 	// Create two stats.Handlers which also implement MetricsRecorder, configure
 	// one as a global dial option and one as a local dial option.
-	mr1 := NewTestMetricsRecorder(t) // What does t assert?
-	mr2 := NewTestMetricsRecorder(t)
+	mr1 := NewTestMetricsRecorder(t, []string{})
+	mr2 := NewTestMetricsRecorder(t, []string{})
 
-	// set global dial options to what they were previously? local + global snapshot and send it around
-	// Expect both to get same emissions?
-
-	// Seems like clear is fine here compared with setting to old state...Doug
-	// had logic for back and forth but I think this is fine...
-
-	defer internal.ClearGlobalDialOptions() // so this just clears it which I guess is fine
+	defer internal.ClearGlobalDialOptions()
 	internal.AddGlobalDialOptions.(func(opt ...grpc.DialOption))(grpc.WithStatsHandler(mr1))
 
 	cc, err := grpc.NewClient(mr.Scheme()+":///", grpc.WithResolvers(mr), grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithStatsHandler(mr2))
@@ -89,64 +75,23 @@ func (s) TestMetricsRecorderList(t *testing.T) {
 		log.Fatalf("Failed to dial: %v", err)
 	}
 	defer cc.Close()
-	// Do I need to make an RPC for this to pull the lb I want...and have a spun up server with a certain address?
-	// register the custom lb by importing
 
-	// There's no peer to connect too...
-	// Also what actually triggers the balancer to build...
-	// and the resolver to resolve? First RPC?
 	tsc := grpc_testing.NewTestServiceClient(cc)
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
-	tsc.UnaryCall(ctx, &grpc_testing.SimpleRequest{}) // wait for ready?
-	/*
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-		defer cancel()
-		ec := pb.NewEchoClient(cc)
-		if err := waitForDistribution(ctx, ec); err != nil {
-			log.Fatalf(err.Error())
-		}
-		fmt.Println("Successful multiple iterations of 1:2 ratio")
-	*/
 
-
-
-	// read off mr1 and mr2's channels?
-
-	// Whatever operation triggered build above, build emits metrics which can
-	// be asserted on here...
-
-	// read off channel....extra layer can do tests like labels etc...
-
-	// and different assertions for the three types of metrics...
-
-	// read from one would block so need to do it in another goroutine
-
-	// or could do it essentially like a buffered sink...
-
-	// to essentially see if an event happened,
-
-	// fork a goroutine from the test...?
-
-	// mr1 // could have a wait for assertion on this
-
-	// either have a helper on mr1 that waits for a certain thing orrrr
-	// just do it here
-
-	// both metrics recorders same assertions...
-
+	// Trigger the recording_load_balancer to build, which will trigger metrics
+	// to record.
+	tsc.UnaryCall(ctx, &grpc_testing.SimpleRequest{})
 
 	mdWant := MetricsData{
-		Handle: (*estats.MetricDescriptor)(intCountHandle), // The handle comes in the lb...just ignore this field?
-
+		Handle: (*estats.MetricDescriptor)(intCountHandle),
 		IntIncr: 1,
-
-		LabelKeys: []string{"int counter label", "int counter optional label"}, // coupled with balancer registration...
-		LabelVals: []string{"int counter label val", "int counter optional label val"}, // emitted from balancer...same package so access to handle...make consts?
+		LabelKeys: []string{"int counter label", "int counter optional label"},
+		LabelVals: []string{"int counter label val", "int counter optional label val"},
 	}
-	// Maybe just get this first assertion working/compiled etc than move onto next...
-	mr1.WaitForInt64Count(ctx, mdWant) // metrics data want...does it really need to wait for it it's sync...
-	mr2.WaitForInt64Count(ctx, mdWant) // same with this one...
+	mr1.WaitForInt64Count(ctx, mdWant)
+	mr2.WaitForInt64Count(ctx, mdWant)
 
 	mdWant = MetricsData{
 		Handle: (*estats.MetricDescriptor)(floatCountHandle),
@@ -156,7 +101,6 @@ func (s) TestMetricsRecorderList(t *testing.T) {
 		LabelKeys: []string{"float counter label", "float counter optional label"},
 		LabelVals: []string{"float counter label val", "float counter optional label val"},
 	}
-	// and scale up all these helpers I guess...
 	mr1.WaitForFloat64Count(ctx, mdWant)
 	mr2.WaitForFloat64Count(ctx, mdWant)
 
@@ -181,12 +125,10 @@ func (s) TestMetricsRecorderList(t *testing.T) {
 	}
 	mr1.WaitForFloat64Histo(ctx, mdWant)
 	mr2.WaitForFloat64Histo(ctx, mdWant)
-	// Should ignore the second gauge call - as labels don't match up so metrics
-	// recorder list should eat it...
 	mdWant = MetricsData{
 		Handle: (*estats.MetricDescriptor)(intGaugeHandle),
 
-		IntIncr: 5, // Should ignore the 7 call since 7 emits wrong number of labels...
+		IntIncr: 5, // Should ignore the 7 metrics recording point emits wrong number of labels.
 
 		LabelKeys: []string{"int gauge label", "int gauge optional label"},
 		LabelVals: []string{"int gauge label val", "int gauge optional label val"},
@@ -194,20 +136,3 @@ func (s) TestMetricsRecorderList(t *testing.T) {
 	mr1.WaitForInt64Gauge(ctx, mdWant)
 	mr2.WaitForInt64Gauge(ctx, mdWant)
 }
-
-// move to internal and balancer to test utils to not populate global
-// namespace...and cleanup, I can send this out as a seperate PR, but needs both
-// for next features...
-
-// so rebase onto in flight PR, squash to one commit though...shouldn't conflict
-// that way can do WRR and RLS immediately...
-
-
-
-// Finish this test with all the assertions...move to internal and I think this is standalone outside clear metrics registry for testing thingy...
-// so can just keep that change...
-
-// Looks like it needs to make an RPC to trigger call...
-
-// Need to upload that 1.65 docker image...
-
--- internal/stats/test/testmetricsrecorder.go
+++ internal/stats/test/testmetricsrecorder.go
@@ -20,31 +20,24 @@ package test
 
 import (
 	"context"
+	"testing"
+
 	"github.com/google/go-cmp/cmp"
 	estats "google.golang.org/grpc/experimental/stats"
 	"google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/stats"
-	"testing"
 )
 
-// test only...? how to not expose this to users...
-
-// have this be seperate that'll make it easier, can refactor later...
-
-// TestMetricsRecorder...
+// TestMetricsRecorder is a MetricsRecorder to be used in tests. It sends
+// recording events on channels and provides helpers to check if certain events
+// have taken place. It also persists metrics data keyed on the metrics
+// descriptor.
 type TestMetricsRecorder struct {
 	t *testing.T
 
-	// 5 maps (with key values somehow accounted for?)
-	intValues   map[*estats.MetricDescriptor]int64 // bucketed into two? maybe bucket by type...
+	// TODO: scale this persisted storage up with label key values.
+	intValues   map[*estats.MetricDescriptor]int64
 	floatValues map[*estats.MetricDescriptor]float64
-	// testing.T...what is this even used for? have assertions on this type
-
-	// send something below or persist something above that is data +
-	// labels...gets a desc (with labels key), incr data,
-
-	// receive from channel, fail on testing.T
-
 
 	intCountCh *testutils.Channel
 	floatCountCh *testutils.Channel
@@ -54,106 +47,68 @@ type TestMetricsRecorder struct {
 
 }
 
-// new which constructs based off state of metrics registry...
-func NewTestMetricsRecorder(t *testing.T) *TestMetricsRecorder {
+func NewTestMetricsRecorder(t *testing.T, metrics []string) *TestMetricsRecorder {
 	tmr := &TestMetricsRecorder{
 		t: t,
 		intValues: make(map[*estats.MetricDescriptor]int64),
 		floatValues: make(map[*estats.MetricDescriptor]float64),
 
-		intCountCh: testutils.NewChannelWithSize(10), // or 1? Should just get 1 from test but WRR/RLS I don't know what triggers it...
-		floatCountCh: testutils.NewChannelWithSize(10), // if I want one testutils.NewChannel()
+		intCountCh: testutils.NewChannelWithSize(10),
+		floatCountCh: testutils.NewChannelWithSize(10),
 		intHistoCh: testutils.NewChannelWithSize(10),
 		floatHistoCh: testutils.NewChannelWithSize(10),
 		intGaugeCh: testutils.NewChannelWithSize(10),
 	}
 
-	// for _, desc := range
-	// estats.MetricDescriptor{}
-
-	// so this fake one somehow needs to be configured with metrics names...
-	var metrics []string
 	for _, metric := range metrics {
 		desc := estats.DescriptorForMetric(estats.Metric(metric))
 		switch desc.Type {
-		case estats.MetricTypeIntCount: // or can make this more granular by making the logic per...
+		case estats.MetricTypeIntCount:
 		case estats.MetricTypeIntHisto:
 		case estats.MetricTypeIntGauge:
-			tmr.intValues[desc] = 0 // persist this still for basic case...
+			tmr.intValues[desc] = 0
 		case estats.MetricTypeFloatCount:
 		case estats.MetricTypeFloatHisto:
 			tmr.floatValues[desc] = 0
-		} // this can import stats...stats doesn't need this...
+		}
 	}
 	return tmr
 }
 
-// record writes to map
-
-// internal test util to not populate exported namespace?
-type MetricsData struct { // metrics data received for all...
-	// Needs to ID the handle...right
+type MetricsData struct {
 	Handle *estats.MetricDescriptor
 
-	IntIncr int64 // Only set for corresponding type...
+	// Only set based on the type of metric. So only one of IntIncr or FloatIncr
+	// is set.
+	IntIncr int64
 	FloatIncr float64
 
-	LabelKeys []string // or make this one kv slice?
+	LabelKeys []string
 	LabelVals []string
+}
 
-	// Anything else?
-} // Lower layer does label length check...
-
-// could add verifications in future PR's which keep reading from these channels
-// (one of the 5 specific for type) and expecting something...
-func (r *TestMetricsRecorder) WaitForInt64Count(ctx context.Context, metricsDataWant MetricsData) { // Metrics recording points come sync? so do I even need to poll
-	// Again can scale up these operations in the future
-	got, err := r.intCountCh.Receive(ctx) // and then what to do with got?
+func (r *TestMetricsRecorder) WaitForInt64Count(ctx context.Context, metricsDataWant MetricsData) {
+	got, err := r.intCountCh.Receive(ctx)
 	if err != nil {
 		r.t.Fatalf("timeout waiting for int64 count")
-	} // send it on this channel or persist it around to verify later...
+	}
 	metricsDataGot := got.(MetricsData)
 	if diff := cmp.Diff(metricsDataGot, metricsDataWant); diff != "" {
 		r.t.Fatalf("int64count metricsData received unexpected value (-got, +want): %v", diff)
 	}
 }
 
-// It's essentially like you persist it around in a buffer to be read later...rather than persist as a slice
-// or w/e
-
-// I don't think needs to do any metrics verifications...
 func (r *TestMetricsRecorder) RecordInt64Count(handle *estats.Int64CountHandle, incr int64, labels ...string) {
-	// r.intCountCh.Send(/*something with value type and key value of labels...I guess labels are something interesting to assert*/)
-
-	// This persistence needs the k/v dimension though...I guess in future...for
-	// certain labels if I want to add that dimension this is iterable and
-	// malleable though...
-
-	// probably need to export this
 	r.intCountCh.Send(MetricsData{
 		Handle: (*estats.MetricDescriptor)(handle),
 		IntIncr: incr,
-
-		LabelKeys: append(handle.Labels, handle.OptionalLabels...), // this verifies some part of instrument registry but do I want more?
-
+		LabelKeys: append(handle.Labels, handle.OptionalLabels...),
 		LabelVals: labels,
+	})
 
-		// Anything else - if so can verify something from instrument
-		// registration...maybe have the 5 registered instruments be consts do
-		// it like inst registry tests eh...
-
-
-	}) // or send context? This just sticks it on buffer right? Like testServer
-
-	// trivial incr persistence, might want to combine hash with handle for label key
-	// specificity, have to provide both handle and hash...
-
-
-
-	r.intValues[(*estats.MetricDescriptor)(handle)] += incr // or in the future could scale up this persistence
+	r.intValues[(*estats.MetricDescriptor)(handle)] += incr
 }
 
-// Exact same logic except receives from different channel/different log statement...
 func (r *TestMetricsRecorder) WaitForFloat64Count(ctx context.Context, metricsDataWant MetricsData) {
 	got, err := r.floatCountCh.Receive(ctx)
 	if err != nil {
@@ -169,17 +124,10 @@ func (r *TestMetricsRecorder) RecordFloat64Count(handle *estats.Float64CountHand
 	r.floatCountCh.Send(MetricsData{
 		Handle: (*estats.MetricDescriptor)(handle),
 		FloatIncr: incr,
-
 		LabelKeys: append(handle.Labels, handle.OptionalLabels...),
-
 		LabelVals: labels,
-
-		// Anything else - if so can verify something from instrument
-		// registration...
-
 	})
 
-	// This persistence needs the k/v dimension though...I guess in future...
 	r.floatValues[(*estats.MetricDescriptor)(handle)] += incr
 }
 
@@ -198,17 +146,10 @@ func (r *TestMetricsRecorder) RecordInt64Histo(handle *estats.Int64HistoHandle, 
 	r.intHistoCh.Send(MetricsData{
 		Handle: (*estats.MetricDescriptor)(handle),
 		IntIncr: incr,
-
 		LabelKeys: append(handle.Labels, handle.OptionalLabels...),
-
 		LabelVals: labels,
-
-		// Anything else - if so can verify something from instrument
-		// registration...
-
 	})
 
-	// This persistence needs the k/v dimension though...I guess in future...
 	r.intValues[(*estats.MetricDescriptor)(handle)] += incr
 }
 
@@ -227,16 +168,9 @@ func (r *TestMetricsRecorder) RecordFloat64Histo(handle *estats.Float64HistoHand
 	r.floatHistoCh.Send(MetricsData{
 		Handle: (*estats.MetricDescriptor)(handle),
 		FloatIncr: incr,
-
 		LabelKeys: append(handle.Labels, handle.OptionalLabels...),
-
 		LabelVals: labels,
-
-		// Anything else - if so can verify something from instrument
-		// registration...hardcode something interesting there?
-
 	})
-	// This persistence needs the k/v dimension though...I guess in future...
 	r.floatValues[(*estats.MetricDescriptor)(handle)] += incr
 }
 
@@ -255,22 +189,14 @@ func (r *TestMetricsRecorder) RecordInt64Gauge(handle *estats.Int64GaugeHandle, 
 	r.intGaugeCh.Send(MetricsData{
 		Handle: (*estats.MetricDescriptor)(handle),
 		IntIncr: incr,
-
 		LabelKeys: append(handle.Labels, handle.OptionalLabels...),
-
 		LabelVals: labels,
-
-		// Anything else - if so can verify something from instrument
-		// registration...
-
 	})
 
-	// This persistence needs the k/v dimension though...I guess in future...
-	r.intValues[(*estats.MetricDescriptor)(handle)] = incr // have this be simply a check on most recent...
+	r.intValues[(*estats.MetricDescriptor)(handle)] = incr
 }
 
-// To make it a stats.Handler:
-
+// To implement a stats.Handler, which allows it to be set as a dial option:
 func (r *TestMetricsRecorder) TagRPC(ctx context.Context, _ *stats.RPCTagInfo) context.Context {
 	return ctx
 }
@@ -282,40 +208,3 @@ func (r *TestMetricsRecorder) TagConn(ctx context.Context, _ *stats.ConnTagInfo)
 }
 
 func (r *TestMetricsRecorder) HandleConn(context.Context, stats.ConnStats) {}
-
-
-// testmetricsrecorder just copy verbatim and export...figure out circular dependency issue? If the estats test doesn't take a dependency on this
-// I think circular dependency problem goes away...
-
-// and verify by reading map from test...Doug seemed fine with this...
-
-// events that happen put it on buffered channel...or channel
-// or callback like stub server?
-
-// build full assertions on top of this...RLS/WRR
-
-
-// Operations write to a channel and higher layers can read the channel as they want...
-
-// Build on top of this, can assert like Yijie or not or keep it lightweight for the test
-
-// 5 channels? Buffered channels?
-// Sync point?
-
-// Read off the 5 and make sure it hits at Dial time?
-
-// What's the best thing to do here?
-
-
-
-
-// buffered channel doesn't block and is a sink
-// maybe do like test client with buffered channel of length 10...
-
-// channels block, but do I have enough granular control of WRR/RLS to always have corresponding reads?
-
-// testutils.Channel is a buffered sink that has extra operations wrapping it...
-
-
-
-
